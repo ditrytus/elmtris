1eeb8da558342a53756af07a12249ae0180af10d
Jakub Gruszecki <jakubgruszecki@MacBook-Pro-Admin-2.local>
2016-12-04 00:28:50 +0100

    Reorganized files

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-elm-make Main.elm --output elmtris.js --yes
 mkdir -pv out
-cp -v elmtris.js index.html style.css coolville-webfont.woff coolville-webfont.woff2 out
+elm-make src/elm/Main.elm --output out/elmtris.js --yes
+cp -v index.html style.css coolville-webfont.woff coolville-webfont.woff2 out
