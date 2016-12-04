7b6ebf015d047631c59ad88408ef20a6567477e1
Jakub Gruszecki <jakubgruszecki@MacBook-Pro-Admin-2.local>
2016-12-04 14:12:05 +0100

    Building with gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    elm = require('gulp-elm'),
+    del = require('del');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var sourceFolder = 'src/';
+var outputFolder = 'dist/'
+var elmMainFile = sourceFolder + 'app/Main.elm';
+var cssFiles = sourceFolder + '**/*.css';
+var htmlFiles = sourceFolder + '**/*.html';
+var fontFiles = sourceFolder + '**/*.woff*';
+var assetsFiles = [cssFiles, htmlFiles, fontFiles];
+
+gulp.task('elm-init', elm.init);
+
+gulp.task('elm', ['elm-init'], function() {
+  return gulp.src(elmMainFile)
+    .pipe(elm.bundle('elmtris.js'))
+    .pipe(gulp.dest(outputFolder))
+});
+
+gulp.task('assets', function() {
+    return gulp.src([cssFiles, htmlFiles, fontFiles])
+      .pipe(gulp.dest(outputFolder));
+});
+
+gulp.task('clean', function() {
+  return del([outputFolder]);
+});
+
+gulp.task('build', ['clean'], function() {
+  gulp.start(['elm', 'assets']);
 });
